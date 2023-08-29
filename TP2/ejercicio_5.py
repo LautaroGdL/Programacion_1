b916752203cbe2bb0ef4bf6ba59d0e9989e7cc42
Lautaro Garcia De Leo <95439072+LautaroGdL@users.noreply.github.com>
2023-08-29 14:19:56 -0300

    Add files via upload

--- TP2/ejercicio_5.py
+++ TP2/ejercicio_5.py
@@ -1,17 +1,19 @@
-def esta_ordenada_ascendente(lista):
-    for i in range(1, len(lista)):
-        if lista[i] < lista[i - 1]:
+import random
+def Create_list(x):
+    list=[]
+    for i in range(x):
+        y=(int(input(" Ingrese los valores deseados a la lista: ")))
+        list.append(y)
+    return list
+def Verif_List(list):
+    for i in range(len (list)-1):
+        if list[i]> list[i+1]:
             return False
-    return True
-
-# Función para verificar el comportamiento de la función
-def verificar_funcion():
-    lista_1 = [1, 2, 3, 4, 5]
-    lista_2 = [5, 4, 3, 2, 1]
-    lista_3 = [1, 3, 2, 4, 5]
-
-    print("Lista 1:", esta_ordenada_ascendente(lista_1))  # Debería imprimir True
-    print("Lista 2:", esta_ordenada_ascendente(lista_2))  # Debería imprimir False
-    print("Lista 3:", esta_ordenada_ascendente(lista_3))  # Debería imprimir False
-
-verificar_funcion()
+        else:
+            return True
+##probar luego con valores aleatorios##
+##main
+N_cant_valores=(int(input(" Ingrese la cantidad de elementos: ")))
+list=Create_list(N_cant_valores)
+list=Verif_List(list)
+print(list)
