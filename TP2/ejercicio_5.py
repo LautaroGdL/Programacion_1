b60400674ffe3babf2fefa1333f542ab5e2c5259
Lautaro Garcia De Leo <95439072+LautaroGdL@users.noreply.github.com>
2023-08-29 15:18:36 -0300

    ejercicio 5 bien hecho

--- TP2/ejercicio_5.py
+++ TP2/ejercicio_5.py
@@ -5,15 +5,27 @@ def Create_list(x):
         y=(int(input(" Ingrese los valores deseados a la lista: ")))
         list.append(y)
     return list
-def Verif_List(list):
-    for i in range(len (list)-1):
-        if list[i]> list[i+1]:
+
+def Verif_List(lista):
+    for i in range(len(lista) - 1):
+        if lista[i] > lista[i + 1]:
             return False
-        else:
-            return True
+    return True
 ##probar luego con valores aleatorios##
+def list_random(x):
+    list1=[]
+    for i in range(x):
+        list1.append(random.randint(0,100))
+    return list1
 ##main
-N_cant_valores=(int(input(" Ingrese la cantidad de elementos: ")))
+
+N_cant_valores=(int(input(" Ingrese la cantidad de elementos que desea en su lista: ")))
 list=Create_list(N_cant_valores)
 list=Verif_List(list)
 print(list)
+
+print()
+list1=list_random(N_cant_valores)
+print(list1)
+list1=Verif_List(list1)
+print(list1)
